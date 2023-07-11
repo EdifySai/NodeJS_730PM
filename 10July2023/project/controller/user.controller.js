fa51724bf77ebad90390ebe79e75e9ed4105fcdd
sai <sai@digital-lync.com>
2023-07-11 18:55:34 +0530

    11july2023

--- 10July2023/project/controller/user.controller.js
+++ 10July2023/project/controller/user.controller.js
@@ -10,3 +10,38 @@ exports.add = async (req, res) => {
         console.log("error", error)
     }
 }
+
+exports.update = async (req, res) => {
+    var body = req.body;
+    try {
+        var response = await UserModel.findOneAndUpdate({ emailid: body.emailid }, body);
+        console.log(response);
+        res.send(response);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
+exports.delete = async (req, res) => {
+    var emailid = req.body.emailid;
+    try {
+        var response = await UserModel.findOneAndDelete({ emailid: emailid });
+        console.log(response);
+        res.send(response);
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+}
+exports.list = async (req, res) => {
+    try {
+        var response = await UserModel.find({});
+        res.send(response);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
--- 10July2023/project/routes/user.route.js
+++ 10July2023/project/routes/user.route.js
@@ -2,4 +2,7 @@ var express = require('express');
 var userController = require('../controller/user.controller');
 var router = express.Router();
 router.post('/add', userController.add);
+router.put("/update", userController.update);
+router.delete("/delete", userController.delete);
+router.get("/list", userController.list);
 module.exports = router;
